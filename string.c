b3929fd36334b6463885eac75c746147e97fdc30
mersigit <mersi.tesfaye18@gmail.com>
2023-05-24 00:05:24 -0700

    string files updated

--- string.c
+++ string.c
@@ -1,27 +1,29 @@
+
 #include "shell.h"
 
 /**
- * _strlen - the function that retuns no. of chars
- * @s: the string argument passed to the function
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: the no. of character in a string
+ * Return: integer length of string
  */
 int _strlen(char *s)
 {
-	int k = 0;
+	int i = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		k++;
-	return (k);
+		i++;
+	return (i);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparision.
- * @s1: the first string argument
- * @s2: the second string argument
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
+ *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
@@ -55,20 +57,21 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - the function that adds two strings together
- * @dest: the first string
- * @src: the second string
- * Return: points to the dest buffer
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
 {
-	char *y = dest;
+	char *ret = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (y);
+	return (ret);
 }
 
--- string1.c
+++ string1.c
@@ -1,85 +1,89 @@
 #include "shell.h"
 
 /**
- * _strcpy - the function that duplicates string
- * @dest: the destination string argument
- * @src: the source string argument
- * Return: points to the destination address
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
  */
 char *_strcpy(char *dest, char *src)
 {
-	int j = 0;
+	int i = 0;
 
 	if (dest == src || src == 0)
 		return (dest);
-	for (; src[j];)
+	while (src[i])
 	{
-		dest[j] = src[j];
-		j++;
+		dest[i] = src[i];
+		i++;
 	}
-	dest[j] = 0;
+	dest[i] = 0;
 	return (dest);
 }
 
 /**
- * _strdup - the function that replicates string
- * @str: the string argument passed to the function
- * Return: points to the address of replicated str
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
  */
 char *_strdup(const char *str)
 {
-	char *r;
-	int l = 0;
+	int length = 0;
+	char *ret;
 
 	if (str == NULL)
 		return (NULL);
 	while (*str++)
-		l++;
-	r = malloc(sizeof(char) * (l + 1));
-	if (!r)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
 		return (NULL);
-	for (l++; l--;)
-		r[l] = *--str;
-	return (r);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
 }
 
 /**
- * _puts - the function that displays an input string
- * @str: the string argument passed to the fun
+ * _puts - prints an input string
+ * @str: the string to be printed
  *
- * Return: void
+ * Return: Nothing
  */
 void _puts(char *str)
 {
-	int j = 0;
+	int i = 0;
 
 	if (!str)
 		return;
-	while (str[j] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(str[j]);
-		j++;
+		_putchar(str[i]);
+		i++;
 	}
 }
 
 /**
- * _putchar - this function writes c to stdout
- * @c: character argument passed to fun
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
  * Return: On success 1.
- * On error, -1 is returned.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	static int k;
-	static char b[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || k >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(1, b, k);
-		k = 0;
+		write(1, buf, i);
+		i = 0;
 	}
 	if (c != BUF_FLUSH)
-		b[k++] = c;
+		buf[i++] = c;
 	return (1);
 }
+
 
