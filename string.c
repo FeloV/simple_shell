e3eb0885d8bf910f6e51d8ef4d670b64a71d3bfc
mersigit <mersi.tesfaye18@gmail.com>
2023-05-23 21:51:08 -0700

    string.c updated

--- string.c
+++ string.c
@@ -1,28 +1,27 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - the function that retuns no. of chars
+ * @s: the string argument passed to the function
  *
- * Return: integer length of string
+ * Return: the no. of character in a string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int k = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		k++;
+	return (k);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
- *
+ * _strcmp - performs lexicogarphic comparision.
+ * @s1: the first string argument
+ * @s2: the second string argument
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
@@ -56,21 +55,20 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
- *
- * Return: pointer to destination buffer
+ * _strcat - the function that adds two strings together
+ * @dest: the first string
+ * @src: the second string
+ * Return: points to the dest buffer
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ret = dest;
+	char *y = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (ret);
+	return (y);
 }
 
